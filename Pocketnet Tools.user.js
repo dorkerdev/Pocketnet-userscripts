066fb9a53990eca139010d2b7bb181ace3d68f49
dorker <uppermosthue@protonmail.com>
2021-08-30 16:51:45 -0400

    Update Pocketnet Tools.user.js

--- Pocketnet Tools.user.js
+++ Pocketnet Tools.user.js
@@ -2,8 +2,8 @@
 // @name         Pocketnet Tools
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Adds a "Show Votes" button
+// @author       dorker
 // @match        https://pocketnet.app/*
 // @match        https://bastyon.com/*
 // @icon         https://www.google.com/s2/favicons?domain=mozilla.org
