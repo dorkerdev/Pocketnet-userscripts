50f9fae09279e0ca11a8024f54e05654100a9ba3
dorker <uppermosthue@protonmail.com>
2023-03-19 02:03:21 -0400

    optimization improvements
    use promises to allow synchronous blocking. execution stops when specific error codes are returned from the server rather than running it unconditionally on the entire address array and returning error after error

--- autoblocker.js
+++ autoblocker.js
@@ -25,30 +25,79 @@ javascript:(()=>{ ..your JS code.. })()
 
 Remove the // from the code below and then paste all into bookmarklet.
 
-TODO: Add some notification to let the user know whether or not the blocks were
-successful. Dev console is currently the only indicator.
-
 NOTE: I believe there is a blocking cool-down limit if your account is < 100 rep. If
 so, you will only be able to block maybe 25 at a time and then have to wait before
-being able to block again.
+being able to block again. You will get the error code "money"... whatever that means.
 */
 
 //javascript:(()=>{
-app.api.rpc("getuserprofile", [[app.platform.sdk.address.pnet().address],"0"]).then(d => {
+//debugger;
+let me = app.platform.sdk.user.me();
+
+/*get the logged-in user profile so you have access to its address and blocks*/
+app.api.rpc("getuserprofile", [[me.address],"0"]).then(async d => {
 	const forEachAsPromise = async (items, action) => {
-		for (const item of items) {
-			await action(item)
+        for (const item of items) {
+			let code;
+            await action(item, x => code = x);
+            if (code) {
+                if (code === "money" || code === 28) return code;
+            }
 		}
 	};
-	const addrs = prompt("Enter addresses to block, each on its own line")
-		.split("\n")
+    var addrs = prompt("Enter addresses to block, each on its own line");
+    /*
+    var addrs = `ahgdhjgshdjssdghdjdsjgh
+    fgflkfkjhfhkjfhjkfhkjk`;
+    */
+    let alreadyBlocked = 0;
+
+    sitemessage("Auto-blocker initiating...");
+    
+    /*
+    sanitize the list by removing blank lines and addresses that have 
+    already been blocked
+    */
+    addrs = addrs
+		.split("\r\n")
 		.filter(x => x)
-		.map(x => x.split(":")[0].trim())
-		.filter(x => !d[0].blocking.includes(x));
-	forEachAsPromise(addrs, addr => {
-		app.platform.api.actions.blocking(addr.trim(), function(e,t) {
-			console.log(e || t);
-		});
-	});
+		.map(x => x.split(":")[0].trim());
+    
+    let addrCount = addrs.length;
+    
+    addrs = addrs.filter(x => {
+        if (d[0].blocking.includes(x)) {
+            alreadyBlocked++;
+            return false;
+        }
+        return true;
+    });
+    
+    let blocks = 0;
+
+    let code = await forEachAsPromise(addrs, async (addr, clbk) => {
+        await new Promise((res,rej) => {
+            app.platform.api.actions.blocking(addr, function(e,t) {
+				/*app.platform.ws.messages*/
+                clbk(t);
+                if (!t && me.relation(addr, "blocking")) {
+                    blocks++;
+                    if (blocks % 10 === 0) sitemessage(`Blocked ${blocks} of ${addrCount}...`);
+                }
+                res(t);
+            });
+        });
+    });
+
+    let msgs = [];
+
+    if (code) {
+        msgs.push(`Blocking failed. Error code: ${code}.`);
+    } 
+    
+    msgs.push(`Blocked ${blocks} out of ${addrs.length} addresses`);
+    if (alreadyBlocked > 0) msgs.push(`Already blocked ${alreadyBlocked} out of ${addrCount} addresses`);
+
+    sitemessage(msgs.join("</br>"));
 });
 //})()
